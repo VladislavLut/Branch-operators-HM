f201a1c63fa833598871607c8afaa013c3053aa1
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-09-20 15:28:26 +0300

    ex-2 was done

--- main.py
+++ main.py
@@ -7,5 +7,35 @@ try:
     if n == 2:
         print('Лютий')
 
+    if n == 3:
+        print('Березень')
+
+    if n == 4:
+        print('Квітень')
+
+    if n == 5:
+        print('Травень')
+
+    if n == 6:
+        print('Червень')
+
+    if n == 7:
+        print('Липень')
+
+    if n == 8:
+        print('Серпень')
+
+    if n == 9:
+        print('Вересень')
+
+    if n == 10:
+        print('Жовтень')
+
+    if n == 11:
+        print('Листопад')
+
+    if n == 12:
+        print('Грудень')
+
 except Exception as e:
     print(e)

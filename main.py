934681d115863df199771cfeb0c8bd71b3cceb68
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-09-20 15:31:18 +0300

    ex-2 in progress

--- main.py
+++ main.py
@@ -1,41 +1,5 @@
 try:
-    n = int(input('Введіть число: '))
-
-    if n == 1:
-        print('Січень')
-
-    if n == 2:
-        print('Лютий')
-
-    if n == 3:
-        print('Березень')
-
-    if n == 4:
-        print('Квітень')
-
-    if n == 5:
-        print('Травень')
-
-    if n == 6:
-        print('Червень')
-
-    if n == 7:
-        print('Липень')
-
-    if n == 8:
-        print('Серпень')
-
-    if n == 9:
-        print('Вересень')
-
-    if n == 10:
-        print('Жовтень')
-
-    if n == 11:
-        print('Листопад')
-
-    if n == 12:
-        print('Грудень')
+    n = int(input())
 
 except Exception as e:
     print(e)

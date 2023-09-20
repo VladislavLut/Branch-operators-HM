452a48e449fb98ca66be90e947fa9704f691e205
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-09-20 15:40:00 +0300

    ex-4 in progress

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 try:
-    n = int(input("Введіть число: "))
+    x = int(input("Введіть перше число: "))
+    y = int(input("Введіть друге число: "))
 
-    if n > 0:
-        print("Number is positive")
-
-    elif n < 0:
-        print("Number is negative")
-
-    elif n == 0:
-        print("Number is equal to zero")
-
-    else:
-        print("Невірне число")
+    if x == y:
+        print("Числа рівні.")
 
 except Exception as e:
     print(e)

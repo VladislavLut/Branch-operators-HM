fa71e1251f7736e7c5d18da25b5f0e51d9fcbc73
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-09-20 15:35:46 +0300

    ex-2 was done

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 try:
-    n = int(input())
+    n = int(input("Введіть число: "))
+
+    if n > 0:
+        print("Number is positive")
+
+    elif n < 0:
+        print("Number is negative")
+
+    elif n == 0:
+        print("Number is equal to zero")
+
+    else:
+        print("Невірне число")
 
 except Exception as e:
     print(e)

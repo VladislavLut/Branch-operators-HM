d03fe6669d8b837f006c57970f64a4015d0e0355
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-09-20 15:43:52 +0300

    ex-4 was done

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ try:
     if x == y:
         print("Числа рівні.")
 
+    elif x < y:
+        print("Перше число: ", x, "Друге число: ", y)
+
+    else:
+        print("Перше число: ", y, "Друге число: ", x)
+
 except Exception as e:
     print(e)
